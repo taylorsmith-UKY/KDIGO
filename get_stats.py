319688a2f57aaec1c0688239a1cf4b53666aecf4
Taylor <taylor.smith2@uky.edu>
2017-11-28 10:02:14 -0500

    initial-example-results
    Initial clustering results for a subset of 300 patients chosen such that 150 were alive at discharge and 150 died in hospital. Getting statistics for visualization now.

--- get_stats.py
+++ get_stats.py
@@ -1,12 +1,10 @@
 from __future__ import division
-import pandas as pd
 import numpy as np
-import datetime
 import re
-from kdigo_funcs import *
+from kdigo_funcs import get_mat
 
 sort_id = 'STUDY_PATIENT_ID'
-inFile = "../DATA/KDIGO_full.xlsx"
+inFile = "/Users/taylorsmith/Google Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/KDIGO_full.xlsx"
 
 '''
 Code:
@@ -18,70 +16,65 @@ Code:
 '''
 
 date_m = get_mat(inFile,'ADMISSION_INDX',[sort_id])
-id_loc=date_m.columns.get_loc("STUDY_PATIENT_ID")
-hosp_locs=[date_m.columns.get_loc("HOSP_ADMIT_DATE"),date_m.columns.get_loc("HOSP_DISCHARGE_DATE")]
-icu_locs=[date_m.columns.get_loc("ICU_ADMIT_DATE"),date_m.columns.get_loc("ICU_DISCHARGE_DATE")]
 adisp_loc = date_m.columns.get_loc('DISCHARGE_DISPOSITION')
 date_m = date_m.as_matrix()
 
-outcome_m = get_mat(inFile,'OUTCOMES',[sort_id])
-disp_loc = outcome_m.columns.get_loc('INDX_DISCHARGE_DISPOSITION')
-dod_loc = outcome_m.columns.get_loc('DECEASED_DATE')
-outcome_m = outcome_m.as_matrix()
 
-dob_m = get_mat(inFile,'DOB',[sort_id])
-dob_loc = dob_m.columns.get_loc('DOB')
-dob_m = dob_m.as_matrix()
+ids = np.loadtxt('result/6clusters_matorder.csv')[:,0]
 
-f = open('result/kdigo_sub_no0.csv','r')
-rids = []
+f = open('kdigo.csv','r')
 kdigos = []
+k_ids = []
 for l in f:
-    rids.append(int(l.split(', ')[0]))
-    kdigos.append([int(float(x)) for x in l.split(',')[1:]])
-    
-
-f = open('result/kdigo_sub_no0_6clusters_matorder.csv','r')
-clusters = []
-ids = []
-f.readline()
-for l in f:
-    ids.append(int(l.rstrip().split(',')[0][1:-1]))
-    clusters.append(int(l.rstrip().split(',')[1]))
-f.close()
-ids = np.array(ids)
+    if l.split(',')[0] in ids:
+        k_ids.append(l.split(',')[0])
+        kdigos.append([int(float(x)) for x in l.split(',')[1:]])
 n_ids = len(ids)
-
-dead_inp = np.zeros(n_ids,dtype=bool)
-death_dur = np.repeat(datetime.timedelta(10000),n_ids)
-
+dead_inp = np.zeros(n_ids,dtype=int)
 kdigo_max = np.zeros(n_ids,dtype=int)
 kdigo_counts = np.zeros([n_ids,5],dtype=int)
 kdigo_pcts = np.zeros([n_ids,5],dtype=int)
+n_alive = 0
+n_lt = 0
+n_gt = 0
+n_xfer = 0
+n_ama = 0
+n_unk = 0
 
 for i in range(len(ids)):
     idx = ids[i]
     #assign death group
-    rows = np.where(date_m[:,0]==idx)[0]
-    for row in rows:
-        str_disp = str(date_m[row,adisp_loc]).upper()
-        if re.search('LESS THAN',str_disp):
-            dead_inp[i]=1
-    
+    row = np.where(date_m[:,0]==idx)[0][0]
+    str_disp = str(date_m[row,adisp_loc]).upper()
+    if re.search('MORE THAN',str_disp):
+        dead_inp[i] = 1
+        n_gt += 1
+    elif re.search('LESS THAN',str_disp):
+        dead_inp[i] = 1
+        n_lt += 1
+    elif re.search('ALIVE',str_disp):
+        n_alive += 1
+    elif re.search('XFER',str_disp) or re.search('TRANS',str_disp):
+        n_xfer += 1
+    elif re.search('AMA',str_disp):
+        n_ama += 1
+    else:
+        n_unk += 1
+
     #get duration of death
     #disch = get_disch_date(date_m,hosp_locs,idx)
     #dod = get_dod(date_m,outcome_m,dod_loc,idx)
     #if dod is not None:
     #    death_dur[i] = dod - disch
-    
+
     #get max and avg kdigo, as well as percent time at each stage
-    kidx = np.where(rids == idx)[0][0]
+    kidx = np.where(k_ids == idx)[0][0]
     kdigo = kdigos[kidx]
     for j in range(5):
         kdigo_counts[i,j] = len(np.where(kdigo == i)[0])
     kdigo_max[i] = np.max(kdigo)
     kdigo_pcts = kdigo_counts[i,:] / np.sum(kdigo_counts[i,:])
-    
+
 #row_lbls = np.array(ids, dtype='|S5')[:, np.newaxis]
 
 #ds = np.hstack((row_lbls,clusters,dead_inp,kdigo_max,kdigo_pcts))
