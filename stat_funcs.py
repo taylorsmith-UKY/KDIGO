af4a0fd6c9ff0cef22953fae82e4d11d9247bdc9
Taylor <taylor.smith2@uky.edu>
2017-12-06 15:50:18 -0500

    add eps los and survival
    Jingwen's initial additions to count number of episodes of AKI, hospital and ICU LOS, and post-discharge survival time

--- stat_funcs.py
+++ stat_funcs.py
@@ -33,6 +33,16 @@ def get_cstats(clustFile,dataPath,outPath):
     k_ids = np.array(k_ids,dtype=int)
     d_ids = np.array(d_ids,dtype=int)
 
+    #load outcome data
+    date_ids=date_m[:,0]
+    date_all_ids=[]
+    for i in range(len(date_ids)):
+        date_all_ids.append(date_ids[i])
+    outcome_ids = outcome_m[:,0]
+    outcome_allid=[]
+    for i in range(len(outcome_ids)):
+        outcome_allid.append(outcome_ids[i])
+
     #arrays to store individual results
     disch_disp = np.zeros(n_ids,dtype=int)
     kdigo_max = np.zeros(n_ids,dtype=int)
@@ -137,14 +147,106 @@ def get_dod(idx,date_m,outcome_m,dod_loc):
         return None
     return dd
 
-#def count_eps(kdigo,timescale,gap):
+#%%
+def count_eps(kdigo,timescale,gap):
+    count = 0#count how many episode
+    mult_0 = 0 #count how many 0s between two non-zero values
+    num_t = gap/timescale#how many time point need to skip
+    total_0=-1
+    #all zero in this list return 0
+    if all(i == 0 for i in kdigo):
+        return count
+    else:
+        count_val=Counter(kdigo)
+        for i,k in count_val.items():
+             if i == 0:
+                total_0 = k
+                break
+        #the list have less 0 (gap/timescale) means no episode count return 1
+        if total_0 < num_t:
+            count = count+1
+        else:
+            for j in range(len(kdigo)):
+                if kdigo[j] !=0:#non zero number appear
+                    if j > num_t+1:#if we find two non-zero have 8 zero in between, the non zero is at non-zero at list have 10 values,so index should be 9
+                        for w in range(j-1,0,-1):#loop back to find the number of 0s
+                            if kdigo[w] !=0:
+                                break
+                            else:
+                                mult_0=mult_0+1
+                        if mult_0 >= num_t:
+                            count+=1
+                            mult_0=0
+        return count
+#%%
+def get_los(PID,date_m,hosp_locs,icu_locs,date_all_ids):
+    #this function will get the first PID index
+    index = date_all_ids.index(PID)
+    hosp_times=[]#store all hospital  times into list with same patient
+    icu_times=[]#store all icu times into a list with same patient
+    temp_hosp=[]
+    temp_icu=[]
+    for i in range(index,len(date_all_ids)):
+        if date_all_ids[i] == index:
+            temp_hosp.append(date_m[i,hosp_locs[0]])
+            temp_hosp.append(date_m[i,hosp_locs[1]])
+            temp_icu.append(date_m[i,icu_locs[0]])
+            temp_icu.append(date_m[i,icu_locs[1]])
+            hosp_times.append(temp_hosp)
+            icu_times.append(temp_icu)
+            temp_hosp=[]
+            temp_icu=[]
+        else:
+            break
+    lst_set_hosp=[]
+    lst_set_icu=[]
+    los_hosp=0
+    los_icu=0
+    union_hosp=[]
+    union_icu=[]
+    for i in range(len(hosp_times)):
+        hosp=[]
+        icu=[]
+        date_hosp1=hosp_times[i]
+        date_hosp2=hosp_times[i+1]
+        date_icu1=icu_times[i]
+        date_icu2=icu_times[i+1]
+        delta_hosp=date_hosp2-date_hosp1
+        delta_icu = date_icu2-date_icu1
+        for j in range(delta_hosp.days+1):
+            hosp.append(hosp_times[i]+timedelta(days=j))
+        for j in range(delta_icu.days+1):
+            icu.append(date_icu1+timedelta(days=j))
+        lst_set_hosp.append(hosp)
+        lst_set_icu.append(icu)
+        i=i+1
+    #find the union of two list
+    for i in range(len(lst_set_hosp)):
+        union_hosp=sorted(list(set(union_hosp) | set(lst_set_hosp[i])))
+        union_icu = sorted(list(set(union_icu) | set(lst_set_icu[i])))
+    los_hosp=len(union_hosp)
+    los_icu = len(union_icu)
+    return los_hosp,los_icu
 
-#def get_los(PID,date_m,hosp_locs,icu_locs):
-
-#def get_survival_time(PID,date_m,hosp_locs,outcome_m,death_loc):
-    #disch = get_disch_date(idx,date_m,hosp_locs)
-    #dod = get_dod(idx,date_m,outcome_m,dod_loc)
-    #return rdelta.relativedelta(dod,disch)
+#%%
+def get_survival_time(PID,date_m,hosp_locs,outcome_m,death_loc,date_all_ids,outcome_allid):
+    #if duration = -1 means no death date provide for this id
+    index_hosp=date_all_ids.index(PID)
+    index_death = outcome_allid.index(PID)
+    max_date=date_m[index_hosp][hosp_locs[1]]
+    duration=-1
+    if len(str(outcome_m[index_death][death_loc])) >5:
+        #death date provide
+        death_date = outcome_m[index_death][death_loc]
+        for i in range(index_hosp,len(date_all_ids)):
+            if date_m[index_hosp][0] == PID:
+                if date_m[index_hosp][hosp_locs[1]] > max_date:
+                    max_date = date_m[index_hosp][hosp_loc[1]]
+            else:
+                break
+        diff = death_date - max_date
+        duration = diff.days
+    return duration
 
 #%% Summarize discharge dispositions for a file
 def get_disch_summary(inFile,statFile,ids=None):
