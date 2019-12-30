a16c728ca096def955ca1505cea3cbc1b9081ae5
Taylor Smith <taylor.smith2@uky.edu>
2019-12-29 19:17:09 -0500

    Added script to run pwiseDist on zero-padded input

---
+++ scripts/zpdist.sh
@@ -0,0 +1,11 @@
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -popDist -dfunc braycurtis -v
+
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -popDist -dfunc euclidean -v
+
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -popDist -dfunc cityblock -v
+
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -dfunc braycurtis -v
+
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -dfunc euclidean -v
+
+pwisedist "kdigo_icu_2ptAvg_14d_zeropad.csv" "kdigo_icu_2ptAvg_transition_weights.csv" "/Volumes/GoogleDrive/My Drive/Documents/Work/Workspace/Kidney Pathology/KDIGO_eGFR_traj/DATA/icu/final" "" -dfunc cityblock -v
